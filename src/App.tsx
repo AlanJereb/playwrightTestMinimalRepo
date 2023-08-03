33a7b142bd4b0e1fe8927cfce874ea107006ce66
BS\alan.jereb <alan.jereb@b-s.si>
2023-08-03 10:35:32 +0200

    .

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,22 @@ import Dropdown from "./Dropdown";
 import "./styles.css";
 
 function App() {
-  const contextMenu = (
-    <img
-      className="image-that-should-render"
-      src="https://cdns.iconmonstr.com/wp-content/releases/preview/2018/240/iconmonstr-random-thin.png"
-      alt=""
-    />
-  );
+  
+  const contextMenu = () => {    
+    const contextMenuItem = (
+      <img
+        className="image-that-should-render"
+        src="https://cdns.iconmonstr.com/wp-content/releases/preview/2018/240/iconmonstr-random-thin.png"
+        alt=""
+        style={{maxHeight: 50, maxWidth: 50}}
+      />
+    );
+    return Array.from(Array(5)).map((_x, index) => {
+      return (
+        <div key={index}>{contextMenuItem}</div>
+      )
+    })
+  }
 
   let _contextMenuToggler: () => void;
   const setContextMenuToggler = (fn: () => void) =>
@@ -26,8 +35,8 @@ function App() {
   return (
     <div className="App">
       <div className={"test-div"} onContextMenu={rightClick}>
-        <p>Right-click here</p>
-        <Dropdown exposeOpen={setContextMenuToggler}>{contextMenu}</Dropdown>
+        <p>To open dropdown right-click here</p>
+        <Dropdown exposeOpen={setContextMenuToggler}>{contextMenu()}</Dropdown>
       </div>
     </div>
   );
--- src/Dropdown.tsx
+++ src/Dropdown.tsx
@@ -2,23 +2,93 @@ import React from "react";
 
 interface Props {
   exposeOpen: (fn: () => void) => () => void;
-  children: React.ReactElement;
+  children: React.ReactElement[];
 }
 
 type State = {
   open: boolean;
 };
 
+const isFullString = (e: any): e is string => typeof e === "string" && e.trim().length > 0; // stupid type guard for TS
+
 class Dropdown extends React.Component<Props, State> {
   state: State = { open: false };
+  _ref = React.createRef<HTMLDivElement>();
 
   componentDidMount() {
+    document.addEventListener("mousedown", this.bodyClick, true);
+    document.addEventListener("mouseup", this.bodyClick, true);
+    document.addEventListener("focusin", this.bodyClick, true);
+    document.addEventListener("contextmenu", this.bodyClick, true);
     if (this.props.exposeOpen) {
       this.props.exposeOpen(this.openClose);
     }
   }
+  componentWillUnmount() {
+    document.removeEventListener("mousedown", this.bodyClick, true);
+    document.removeEventListener("mouseup", this.bodyClick, true);
+    document.removeEventListener("focusin", this.bodyClick, true);
+    document.removeEventListener("contextmenu", this.bodyClick, true);
+    if (this.props.exposeOpen) {
+      this.props.exposeOpen(() => {});
+    }
+  }
 
-  openClose = () => {
+  eventWasOutside(
+    e?: React.MouseEvent | MouseEvent | FocusEvent,
+    exclusions?: Array<string | React.RefObject<HTMLElement>>,
+  ): boolean {
+    if (!e) {
+      return false;
+    }
+    const nodeTarget = e.target as Node;
+    if (!nodeTarget?.isConnected) {
+      return false;
+    }
+    exclusions = [...(exclusions || []), ".resizerule", ".ui-block"];
+    const isWithin = (el: Element | undefined | null): boolean =>
+      Boolean(el && (el.contains(nodeTarget) || el === nodeTarget))
+    ;
+    const stringExclusions = exclusions.filter(isFullString);
+    for (const exclusion of exclusions || []) {
+      if (!exclusion || typeof exclusion === "string") {
+        continue;
+      }
+      if (isWithin(exclusion?.current)) {
+        return false;
+      }
+    }
+    if (stringExclusions.length) {
+      const matches = document.querySelectorAll(stringExclusions.join(","));
+      // tslint:disable-next-line: prefer-for-of
+      for (let i = 0; i < matches.length; i++) {
+        if (isWithin(matches[i])) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  mouseDownWasOutside: boolean | undefined;
+  bodyClick = (e: MouseEvent | FocusEvent) => {
+    if (
+      !this.state.open ||
+      !this.eventWasOutside(e, [this._ref]) ||
+      this.mouseDownWasOutside === false
+    ) {
+      if (e.type === "mousedown") {
+        this.mouseDownWasOutside = this.eventWasOutside(e, [this._ref]);
+      }
+      return;
+    }
+    if (e.type === "mousedown") {
+      return;
+    }
+    this.openClose(false);
+  }
+
+  openClose = (open?: boolean) => {
     const newState = !this.state.open;
     if (newState !== this.state.open) {
       this.setState({
@@ -29,7 +99,12 @@ class Dropdown extends React.Component<Props, State> {
 
   render() {
     return this.state.open ? (
-      <div onClick={this.openClose}>
+      <div onClick={() => this.openClose()} ref={this._ref} style={{
+        display: "flex",
+        flexDirection: "column",
+        border: "1px solid black",
+        maxWidth: 300,
+      }}>
         {this.props.children}
       </div>
     ) : null;
